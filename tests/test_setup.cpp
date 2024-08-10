e605f9b4a1669d13ffe62b6699eec54f23d6b791
Raphaël FLEURY-LE VESO <mail@rflv.fr>
2024-08-11 01:05:25 +0200

    Added testing workflow

---
+++ tests/test_setup.cpp
@@ -0,0 +1,17 @@
+//
+// Created by Administrator on 11/08/2024.
+//
+#include <gtest/gtest.h>
+
+TEST(SetupTest, CreateVirtualMachine) {
+  ASSERT_EQ(true, true);
+}
+
+TEST(SetupTest, StartCreateVirtualMachine) {
+  ASSERT_EQ(true, true);
+}
+
+int main(int argc, char **argv) {
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
